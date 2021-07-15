4fb1801518b5dcb08fa8a9023f8a3788b1e96784
bscottfrazier <87489558+bscottfrazier@users.noreply.github.com>
2021-07-15 16:58:57 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,16 @@
-provider "aws" {
-  region = var.region
-}
+# provider "aws" {
+#   region = var.region
+# }
 
-module "s3-webapp" {
-  source  = "app.terraform.io/hashicorp-learn/s3-webapp/aws"
-  name        = var.name
-  region = var.region
-  prefix = var.prefix
+# module "s3-webapp" {
+#   source  = "app.terraform.io/scottf/s3-webapp/aws"
+#   name        = var.name
+#   region = var.region
+#   prefix = var.prefix
+#   version = "1.0.0"
+# }
+module "private-registry-poc" {
+  source  = "app.terraform.io/scottf/private-registry-poc/google"
   version = "1.0.0"
+  # insert required variables here
 }
