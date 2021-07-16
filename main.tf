c1b45d694f62fb070437ce76a6906d5316368674
bscottfrazier <87489558+bscottfrazier@users.noreply.github.com>
2021-07-16 10:23:13 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,3 @@
-# provider "aws" {
-#   region = var.region
-# }
-
-# module "s3-webapp" {
-#   source  = "app.terraform.io/scottf/s3-webapp/aws"
-#   name        = var.name
-#   region = var.region
-#   prefix = var.prefix
-#   version = "1.0.0"
-# }
 module "private-registry-poc" {
   source  = "app.terraform.io/scottf/private-registry-poc/google"
   version = "1.0.1"
