7326c09c8701da6548c17a262fad7faa96d165ad
bscottfrazier <87489558+bscottfrazier@users.noreply.github.com>
2021-07-16 10:47:29 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,9 @@
 module "private-registry-poc" {
   source  = "app.terraform.io/scottf/private-registry-poc/google"
   version = "1.0.2"
+  project = var.project
+  credentials = var.credentials
+  region = var.region
+  zone = var.zone
   # insert required variables here
 }
