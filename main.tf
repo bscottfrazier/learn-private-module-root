6b2059820836394fead100a7a17c995d17d93ae7
bscottfrazier <87489558+bscottfrazier@users.noreply.github.com>
2021-07-16 10:22:13 -0500

    updated version of module being called

--- main.tf
+++ main.tf
@@ -11,6 +11,6 @@
 # }
 module "private-registry-poc" {
   source  = "app.terraform.io/scottf/private-registry-poc/google"
-  version = "1.0.0"
+  version = "1.0.1"
   # insert required variables here
 }
